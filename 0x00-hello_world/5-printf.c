c1b343ec4975b286b16ab9caa352c25af6744d39
Gideon <ben@gmail.com>
2022-09-08 08:27:29 -0700

    Quick Push

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammar%c but the outcome is a piece of art%c", ',', ',');
+	return (0);
+}
