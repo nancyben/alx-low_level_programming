d0dbe77cb80df8fca3a17ef5b64206c383a85de2
Gideon <ben@gmail.com>
2022-09-08 09:28:28 -0700

    Quick Push

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -19,7 +19,7 @@ int main(void)
 	float floatType;
 
 	printf("Size of a char: %zu byte(s)\n", sizeof(charType));
-	printf("Size of a int: %zu byte(s)\n", sizeof(intType));
+	printf("Size of an int: %zu byte(s)\n", sizeof(intType));
 	printf("Size of a long int: %zu byte(s)\n", sizeof(longIntType));
 	printf("Size of a long long int: %zu byte(s)\n", sizeof(longLongIntType));
 	printf("Size of a float: %zu byte(s)\n", sizeof(floatType));
