52094ccc5f6a5f1d5493e620b34e93ba1c295cbb
Gideon <ben@gmail.com>
2022-09-09 10:14:37 -0700

    Quick Push

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+* main -  main program
+*
+* Description: prints lower case letters a - z
+*
+* Return: 0 indicates success
+*/
+int main(void)
+{
+	printf("0123456789\n");
+	return (0);
+}
