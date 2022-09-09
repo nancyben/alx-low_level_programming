8f3c0bbf249cb56d3c4b843e33f9db3aaf66360e
Gideon <ben@gmail.com>
2022-09-09 10:08:08 -0700

    Quick Push

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+* main -  main program
+*
+* Description: prints lower case letters a - z
+*
+* Return: 0 indicates success
+*/
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a' ; ch <= 'z' ; ch++)
+	{
+		putchar(ch);
+	}
+	
+	for (ch = 'A' ; ch <= 'Z' ; ch++)
+        {
+                putchar(ch);
+        }
+
+	printf("\n");
+	return (0);
+}
