492231b0b03bbf841cdb61e8f2bd5cf1ab11857b
Gideon <ben@gmail.com>
2022-09-09 09:55:52 -0700

    Quick Push

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -17,7 +17,7 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 
-	lastDigit = n % 10;
+	ltDigit = n % 10;
 
 	if (lastDigit == 0)
 		printf("Last digit of %d is %d and is 0\n", n, lastDigit);
