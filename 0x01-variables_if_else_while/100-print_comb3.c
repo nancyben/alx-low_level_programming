701aeea8f3a6143d80e457bcd2af11d954c70c80
Gideon <ben@gmail.com>
2022-09-09 16:10:43 -0700

    Quick Push

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+/**
+* main -  main program
+* Print numbers from 0 - 99
+* Return: 0 indicates success
+*/
+int main(void)
+{
+	int rtc;
+
+	for (rtc = 0; rtc <= 99; rtc++)
+	{
+		putcharByPass(rtc / 10 + '0');
+		putcharByPass(rtc % 10 + '0');
+
+		if (rtc < 99)
+		{
+			putcharByPass(',');
+			putcharByPass(' ');
+		}
+	}
+
+	putcharByPass('\n');
+	return (0);
+}
+
+/**
+ * putcharByPass - bypass number of usesable putchar which is 5
+ *
+ * val - char you want to print
+ * Just want to use putchar any how I want :v
+ *  Just for fun :v
+ * Return: void
+ */
+void putcharByPass(char val)
+{
+	putchar(val);
+}
