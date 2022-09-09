e12a593ac8c0761e0e004609fd6389a2c037fdc1
Gideon <ben@gmail.com>
2022-09-09 15:13:54 -0700

    Quick Push

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+* main -  main program
+*
+* Description: prints lower case letters a - z
+*
+* Return: 0 indicates success
+*/
+int main(void)
+{
+	int rtc;
+
+
+	for (rtc = 0; rtc < 10; rtc++)
+	{
+		putchar(rtc + '0');
+		if (rtc < 9)
+			putchar(', ')
+	}
+
+	putchar('\n');
+	return (0);
+}
