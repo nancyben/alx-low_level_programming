1b7ee0210d69caeb6c98e4d73c6037cb01551cff
Gideon <ben@gmail.com>
2022-09-09 09:31:10 -0700

    Quick Push

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -17,11 +17,11 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 
 	if (n == 0)
-		printf("%d is %s", n, "0");
+		printf("%d is zero", n);
 	else if (n > 0)
-		printf("%d is %s", n, "positive");
+		printf("%d is positive", n);
 	else
-		printf("%d is %s", n, "negative");
+		printf("%d is negative", n);
 
 	return (0);
 }
