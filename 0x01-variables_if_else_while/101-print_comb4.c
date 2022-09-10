a745b9cc7abe3b58778a7d64dd205f1b0f3f6439
Gideon <ben@gmail.com>
2022-09-09 17:32:16 -0700

    Quick Push

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+/**
+* main -  main program
+* Print all combination of 3 digit numbers
+* Return: 0 indicates success
+*/
+int main(void)
+{
+	int rtc;
+	int first;
+	int last;
+	int mid;
+
+	for (rtc = 10; rtc < 1000; rtc++)
+	{
+		first = rtc / 100;
+		mid = (rtc / 10) % 10;
+		last  = rtc % 10;
+
+		if (
+				first != mid != last && first != last &&
+				first < mid && first < last && mid < last
+		)
+		{
+			putchar(first + '0');
+			putchar(mid + '0');
+			putchar(last + '0');
+
+			if (rtc < 789)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+
+	putchar('\n');
+	return (0);
+}
