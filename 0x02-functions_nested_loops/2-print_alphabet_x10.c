98085b4220705161beed92f48e7e8f25c930a8d0
Gideon <ben@gmail.com>
2022-09-13 06:17:57 -0700

    Quick Push

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+void print_alphabet(void);
+
+/**
+ * print_alphabet - prints alphabets a - z
+ *
+ * Return: Always void.
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		print_alphabet();
+	}
+}
