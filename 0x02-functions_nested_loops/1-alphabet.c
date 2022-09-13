888e4410a71eba6215469427c9332ff3e7424a8b
Gideon <ben@gmail.com>
2022-09-13 05:16:48 -0700

    Quick Push

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,15 +1,6 @@
 #include "main.h"
 
-void print_alphabet()
-{
-        int i;
-
-        for ( i = 0; i <= 27; i++)
-        {
-                _putchar(97 + i);
-        }
-        _putchar('\n');
-}
+void print_alphabet(void);
 
 /**
  * main - check the code
@@ -20,4 +11,15 @@ int main(void)
 {
     print_alphabet();
     return (0);
+}
+
+void print_alphabet(void)
+{
+        int i;
+
+        for ( i = 0; i <= 27; i++)
+        {
+                _putchar(97 + i);
+        }
+        _putchar('\n');
 }
