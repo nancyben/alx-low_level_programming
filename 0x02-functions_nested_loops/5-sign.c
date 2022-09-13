32026baf5b76dc5a09d27c53490e4f03721baf37
Gideon <ben@gmail.com>
2022-09-13 08:29:34 -0700

    Quick Push

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -10,12 +10,18 @@ void _putchar(char);
 int print_sign(int n)
 {
 	if (n >= 1)
+	{
 		_putchar('+');
 		return (1);
+	}
 	else if (n == 0)
+	{
 		_putchar('0');
 		return (0);
+	}
 	else
+	{
 		_putchar('-');
-		return (-1)
+		return (-1);
+	}
 }
