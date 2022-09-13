1054f6ac12aaf2a115435a5e3bfff2b1c657957e
Gideon <ben@gmail.com>
2022-09-13 07:15:48 -0700

    Quick Push

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,7 +2,9 @@ void _putchar(char);
 
 /**
  * _islower - checks if a character is lowecased or not
- * c - character to check
+ *
+ * @c: character to check
+ *
  * Return: returns 0 if lowercased otherwise return 1
  */
 int _islower(char c)
