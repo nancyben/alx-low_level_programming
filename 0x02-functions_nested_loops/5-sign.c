fd53494086d1c4029d0b262bfa87209ef5a81d76
Gideon <ben@gmail.com>
2022-09-13 08:23:49 -0700

    Quick Push

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,16 @@
+void _putchar(char);
+
+/**
+ * print_sign - checks if a character is lowecased or not
+ *
+ * @c: character to check
+ *
+ * Return: returns 0 if lowercased otherwise return 1
+ */
+int print_sign(char c)
+{
+	if (c >= 97)
+		return (1);
+	else
+		return (0);
+}
