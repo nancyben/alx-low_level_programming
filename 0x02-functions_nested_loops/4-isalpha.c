e86a1860261330b7d9ae5bbe1abb85625d0aefc5
Gideon <ben@gmail.com>
2022-09-13 08:18:58 -0700

    Quick Push

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -9,7 +9,7 @@ void _putchar(char);
  */
 int _isalpha(char c)
 {
-	if (c >= 65 && c <= 90 || c >= 97 && c <= 122)
+	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
 		return (1);
 	else
 		return (0);
