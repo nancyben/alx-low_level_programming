da661eaa8bc07ebf4ce9318ad8e4feeefc6c6b3f
Gideon <ben@gmail.com>
2022-09-13 08:18:05 -0700

    Quick Push

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+void _putchar(char);
+
+/**
+ * _isalpha - checks if a character is alphabet or not
+ *
+ * @c: character to check
+ *
+ * Return: returns 0 if alphabet otherwise return 1
+ */
+int _isalpha(char c)
+{
+	if (c >= 65 && c <= 90 || c >= 97 && c <= 122)
+		return (1);
+	else
+		return (0);
+}
---
+++ 0x02-functions_nested_loops/test.c
@@ -0,0 +1,6 @@
+
+char xter;
+
+xter = 'A';
+
+
