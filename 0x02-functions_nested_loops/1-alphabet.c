efa7d6244f54957490ecad8506b686c76bdc241c
Gideon <ben@gmail.com>
2022-09-13 05:39:25 -0700

    Quick Push

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,18 +1,8 @@
-#include "main.h"
-
-void print_alphabet(void);
-
 /**
  * main - check the code
  *
  * Return: Always 0.
  */
-int main(void)
-{
-    print_alphabet();
-    return (0);
-}
-
 void print_alphabet(void)
 {
         int i;
