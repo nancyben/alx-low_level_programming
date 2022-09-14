5cedeb5528ae8412fcfcd5089ecb53f95f527572
Gideon <ben@gmail.com>
2022-09-13 19:28:43 -0700

    Quick Push

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * add - checks if a character is lowecased or not
+ *
+ *
+ * Return: returns 0 if lowercased otherwise return 1
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}	
