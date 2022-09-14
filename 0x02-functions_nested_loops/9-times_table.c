e0445e28a4c6fce355283a326a9b514f4f76cf0b
Gideon <ben@gmail.com>
2022-09-13 18:55:22 -0700

    Quick Push

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * times_table - prints the 9 times table, starting with 0
+ *
+ *
+ * Return: returns 0 if lowercased otherwise return 1
+ */
+void times_table(void)
+{
+	int leftTimes;
+	int rightTimes;
+
+	for (leftTimes = 0; leftTimes <= 9; leftTimes++)
+	{
+		int result;
+		for (rightTimes = 0; rightTimes <= 9; rightTimes++)
+		{
+			result = leftTimes * rightTimes;
+			if (result > 9)
+				_putchar((result / 10) + '0');
+			_putchar((result % 10) + '0');
+			_putchar(' ');
+			if (rightTimes < 9)
+			{
+				_putchar(',');
+			}
+
+		}
+		_putchar('\n');
+	}
+}
