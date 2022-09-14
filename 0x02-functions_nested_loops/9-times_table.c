6eee64b190bfb022430528acdbc77b72095bdcc2
Gideon <ben@gmail.com>
2022-09-13 19:22:07 -0700

    Quick Push

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -14,18 +14,19 @@ void times_table(void)
 	for (leftTimes = 0; leftTimes <= 9; leftTimes++)
 	{
 		int result;
+
 		for (rightTimes = 0; rightTimes <= 9; rightTimes++)
 		{
 			result = leftTimes * rightTimes;
 			if (result > 9)
 				_putchar((result / 10) + '0');
-			else if(rightTimes > 0)
+			else if (rightTimes > 0)
 				_putchar(' ');
 			_putchar((result % 10) + '0');
 			if (rightTimes < 9)
 			{
 				_putchar(',');
-				_putchar(' ');
+				_putchar(' '):;
 			}
 
 		}
