c810ead0be9a7e64aa9172e3e36d38c287e5821f
Gideon <ben@gmail.com>
2022-09-13 18:30:00 -0700

    Quick Push

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * jack_bauer - checks if a character is lowecased or not
+ *
+ *
+ * Return: returns 0 if lowercased otherwise return 1
+ */
+void jack_bauer(void)
+{
+	int hour, min;
+
+	for (hour = 0; hour < 24; hour++)
+	{
+		for (min = 0; min < 60; min++) 
+		{
+			_putchar((hour / 10) + '0');
+			_putchar((hour % 10) + '0');
+			_putchar(':');
+			_putchar((min / 10) + '0');
+			_putchar((min % 10) + '0');
+			_putchar('\n');
+		}
+	}
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,11 +1,3 @@
-/**
- * _putchar - writes the character c to stdout
- *
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
 int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
@@ -14,3 +6,4 @@ int _isalpha(char);
 int print_sign(int n);
 int _abs(int);
 int print_last_digit(int);
+void jack_bauer(void);
