27f028f45e75180dcbdaee03d1268935720ae305
Gideon Onwuka <dongidomed@gmail.com>
2022-09-15 22:49:10 +0100

    super

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_line - print_line
+ * 
+ * @n: n
+ *
+ * Return: nothing
+ */
+void print_line(int n)
+{
+    int i;
+
+    for (i = 0; i < n; i++)
+    {
+        _putchar('_');
+    }
+
+    _putchar('\n');
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -2,3 +2,4 @@ void print_triangle(int size);
 void _putchar(char);
 void print_square(int size);
 void print_diagonal(int n);
+void print_line(int n);
