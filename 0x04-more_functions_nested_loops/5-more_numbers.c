bd731221884008b07d75ac15641d589e4226ed09
Gideon Onwuka <dongidomed@gmail.com>
2022-09-15 23:53:24 +0100

    super

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * more_numbers - more_numbers
+ *
+ * Return: void
+ */
+
+void more_numbers(void)
+{
+    int i, j;
+
+    for (i = 0; i < 10; i++)
+    {
+        for (j = 0; j < 15; j++)
+        {
+            _putchar((j % 10) + '0');
+
+            if (j > 9)
+            {
+                _putchar((j % 10) + '0');
+            }
+        }
+        _putchar('\n');
+    }
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -3,3 +3,4 @@ void _putchar(char);
 void print_square(int size);
 void print_diagonal(int n);
 void print_line(int n);
+void more_numbers(void);
