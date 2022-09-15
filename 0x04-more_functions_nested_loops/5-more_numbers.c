01c5e4bd991db0d2aeadfd33ce6ff88993cf6296
Gideon <ben@gmail.com>
2022-09-15 16:03:57 -0700

    Quick Push

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,26 +1,27 @@
 #include "main.h"
 
 /**
- * more_numbers - more_numbers
- *
- * Return: void
- */
+* more_numbers - more_numbers
+*
+* Return: void
+*/
 
 void more_numbers(void)
 {
-    int i, j;
+	int i, j;
 
-    for (i = 0; i < 10; i++)
-    {
-        for (j = 0; j < 15; j++)
-        {
-            _putchar((j > 9 ? (j / 10) : j) + '0');
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 15; j++)
+		{
+			_putchar((j > 9 ? (j / 10) : j) + '0');
 
-            if (j > 9)
-            {
-                _putchar((j % 10) + '0');
-            }
-        }
-        _putchar('\n');
-    }
+			if (j > 9)
+			{
+				_putchar((j % 10) + '0');
+			}
+		}
+		_putchar('\n');
+	}
 }
+
