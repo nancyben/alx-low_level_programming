2336a005d25ade46aacd05c722e1d372efc3f2a0
Gideon <ben@gmail.com>
2022-09-19 09:22:11 -0700

    Quick Push

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,4 +1,5 @@
 #include <string.h>
+#include <stdio.h>
 /**
  * print_rev - print_rev
  *
@@ -8,5 +9,15 @@
  */
 void print_rev(char *s)
 {
-	printf("%s", strrev(s));
+	int l;
+
+	l = strlen(s) - 1;
+
+	while (l >= 0)
+	{
+		putchar(s[l]);
+		l--;
+	}
+
+	putchar('\n');
 }
