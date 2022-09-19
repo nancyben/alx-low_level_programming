ca2dc47dc705915c61f5369bbf67a10c85ce7c7e
Gideon <ben@gmail.com>
2022-09-19 08:18:08 -0700

    Quick Push

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+/**
+ * swap_int - swap_int
+ *
+ * @a: 1st input
+ * @b: second input
+ *
+ * Return: void
+ */
+void swap_int(int *a, int *b)
+{
+	int temp;
+	temp = *a;
+
+	*a = *b;
+	*b = temp;
+}
