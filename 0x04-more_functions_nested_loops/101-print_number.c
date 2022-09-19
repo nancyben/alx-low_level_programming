b78f02a9f3a0cf06c91299696e2d2d2e03d26ede
Gideon <ben@gmail.com>
2022-09-19 08:00:12 -0700

    Quick Push

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,10 +1,45 @@
 #include "main.h"
+
 /**
+*print_number - print_number
 *
+* @n: n
 *
+* Return: void
 *
 */
 void print_number(int n)
 {
-    int n, divider;
+	int divider;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n
+	}
+
+	if (n >= 1000)
+	{
+		divider = 1000;
+	}
+	else if (n >= 100)
+	{
+		divider = 100;
+	}
+	else if (n >= 10)
+	{
+		divider = 10;
+	}
+	else
+	{
+		divider = 1;
+	}
+
+	while (divider)
+	{
+		_putchar((n / divider) + '0');
+		n = n % divider;
+		divider = divider / 10;
+	}
+
 }
