32b985c6c53ee7ba9dc63040dbc7005185784646
Gideon <ben@gmail.com>
2022-09-28 10:06:34 -0700

    Quick Push

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -10,10 +10,11 @@
  */
 void _puts_recursion(char *s)
 {
-	if (*s != '\0') {
+	if (*s != '\0')
+	{
 		putchar(*s);
-                _puts_recursion(s + 1);
-	} 
+		_puts_recursion(s + 1);
+	}
 	else
 	{
 		putchar('\n');
