844cb43b52d3d9235956101d7b20afeffa45bf25
Gideon <ben@gmail.com>
2022-09-28 17:14:09 -0700

    Quick Push

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,21 @@
+/**
+ * factorial - factorial
+ *
+ * @n: n
+ *
+ * Return: int
+ */
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+
+	if (n == 0)
+	{
+		return (1);
+	}
+
+	return (n * factorial(n - 1));
+}
