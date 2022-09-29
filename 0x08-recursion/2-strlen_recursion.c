66ba01476e11732d91e1f6bd3a40991f2201fc16
Gideon <ben@gmail.com>
2022-09-28 17:05:47 -0700

    Quick Push

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,12 @@
+/**
+ * _strlen_recursion - recursion
+ *
+ * @s: s
+ *
+ * Return: the s length
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s != '\0')
+		return (1 +  _strlen_recursion(s + 1));
+}
