0be77a0aca1037549fa1709cc0268657b41df686
Gideon <ben@gmail.com>
2022-09-30 05:52:17 -0700

    Quick Push

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - main program function
+ *
+ * @argc - argument count
+ *
+ * @argv - array of strings containing arguments
+ *
+ * Return: 0 to indicate success
+ */
+int main(int argc, char* argv[])
+{
+	argc = argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
