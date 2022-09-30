04805b4b36314b22056a095d5ec0918b3474893a
Gideon <ben@gmail.com>
2022-09-30 05:54:27 -0700

    Quick Push

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -3,13 +3,13 @@
 /**
  * main - main program function
  *
- * @argc - argument count
+ * @argc: argument count
  *
- * @argv - array of strings containing arguments
+ * @argv: array of strings containing arguments
  *
  * Return: 0 to indicate success
  */
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
 	argc = argc;
 	printf("%s\n", argv[0]);
